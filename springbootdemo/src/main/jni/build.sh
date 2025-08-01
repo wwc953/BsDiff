3281c9430abe3d3d01f787292e2cd848e8cebe2e
wangwc <wwc953@163.com>
2025-08-01 17:22:18 +0800

    jni构建脚本

---
+++ springbootdemo/src/main/jni/build.sh
@@ -0,0 +1,2 @@
+cd ./jni
+gcc -fPIC -I/Library/Java/JavaVirtualMachines/jdk1.8.0_251.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_251.jdk/Contents/Home/include/darwin  -shared -o libbsdiff.so org_example_springbootdemo_BsdiffUtils.c
