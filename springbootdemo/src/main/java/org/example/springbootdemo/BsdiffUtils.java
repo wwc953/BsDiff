d8f598beab6b1cbdcdb51497027acf7c53ebc9f1
wangwc <wwc953@163.com>
2025-08-04 15:06:28 +0800

    动态库加载，mac和linux下库的后缀区别,以及编译时需要新增lib前缀

--- springbootdemo/src/main/java/org/example/springbootdemo/BsdiffUtils.java
+++ springbootdemo/src/main/java/org/example/springbootdemo/BsdiffUtils.java
@@ -19,10 +19,19 @@ public class BsdiffUtils {
     public native static int bspatch(String oldfile, String newfile, String patchfile);
 
     static {
-//        System.load("/Users/wangwc/IdeaProjects/BsDiff/springbootdemo/src/main/resources/lib/bsdiff.so");
-//        System.load("/Users/wangwc/IdeaProjects/BsDiff/springbootdemo/src/main/resources/lib/bspatch.so");
 
-        System.load("/Users/wangwc/IdeaProjects/BsDiff/springbootdemo/src/main/resources/lib/libbsdiff.so");
+                System.out.println("======"+System.getProperty("java.library.path"));
+
+        //System.load  so文件绝对路径
+//        System.load("/Users/wangwc/IdeaProjects/BsDiff/springbootdemo/src/main/resources/lib/libbsdiff.so");
+
+        /**
+         * 动态链接
+         * 1、linux -> *.so  mac -> *.jnilib   jnilib将so改后缀即可
+         * 2、System.loadLibrary加载去除名称前面的lib和后缀
+         * 3、运行时指定java.library.path  java -Djava.library.path=/Users/wangwc/IdeaProjects/BsDiff/springbootdemo/src/main/resources/libmac -jar xxx.jar
+         */
+        System.loadLibrary("bsdiff");
 
     }
 
--- springbootdemo/src/main/jni/build.sh
+++ springbootdemo/src/main/jni/build.sh
@@ -1,2 +1,3 @@
 cd ./jni
+#so名称前需要加lib   libxxx.so
 gcc -fPIC -I/Library/Java/JavaVirtualMachines/jdk1.8.0_251.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_251.jdk/Contents/Home/include/darwin  -shared -o libbsdiff.so org_example_springbootdemo_BsdiffUtils.c
